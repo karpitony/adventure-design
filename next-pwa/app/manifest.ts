fe1d78d20afa06822bdbf3edb468ec629c01b209
Yunseok <watemark1017@gmail.com>
2024-11-25 02:11:54 +0900

    구버전 삭제

--- next-pwa/app/manifest.ts
+++ next-pwa/app/manifest.ts
@@ -3,6 +3,7 @@ import { MetadataRoute } from "next";
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "어벤디 차수판",
+    short_name: "어벤디 차수판",
     description: "PWA for 어벤디 4T조",
     display: "standalone",
     start_url: "/",
