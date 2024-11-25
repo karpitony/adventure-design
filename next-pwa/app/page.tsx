0c7b70c9bbf637b491067914781f0c367d90f8d3
Yunseok <watemark1017@gmail.com>
2024-11-25 15:49:04 +0900

    텍스트 수정

--- next-pwa/app/page.tsx
+++ next-pwa/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
   return (
     <div className="w-full text-white">
       <h3 className="mt-4 text-base min-[372px]:text-lg sm:text-xl leading-none font-bold tracking-tight text-purple-200">
-        <span className="text-[gold] opacity-75">어드벤쳐 디자인 N분반</span> @4T조
+        <span className="text-[gold] opacity-75">어드벤쳐 디자인 1분반</span> @4T조
       </h3>
       <h1 className="text-4xl min-[372px]:text-5xl sm:text-6xl font-extrabold tracking-tight mb-4 sm:mb-6 text-purple-400">
         스마트 차수판 웹앱
@@ -39,7 +39,7 @@ export default function Home() {
             url="TBD"
           />
           <LinkCard
-            icon={<TbBrandGithub className="text-gray-500" />}
+            icon={<TbBrandGithub className="text-gray-400" />}
             title="소스 코드"
             url="https://github.com/karpitony/adventure-design"
           />
